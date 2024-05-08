dacf7e5acd46a9fd0723369fb7562259c9b46f1d
idil <idil.iman1999@gmail.com>
2024-05-08 16:52:56 +0800

    chore: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -20,6 +22,7 @@ export default function RootLayout({
       <body className={font.className}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
