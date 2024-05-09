b316c041dcedb06642e1d41f0be4f5d007a8298f
idil <idil.iman1999@gmail.com>
2024-05-09 20:40:03 +0800

    style: update mobile nav

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -11,11 +11,11 @@ export default function MobileNavbar({ className }: Props) {
   return (
     <div
       className={cn(
-        'w-full h-[60px] fixed top-0 z-50 backdrop-blur-sm bg-opacity-10 bg-zinc-100 rounded-sm backdrop-filter',
+        'w-full h-[60px] fixed top-0 z-50 backdrop-blur-sm bg-opacity-10 bg-zinc-100 rounded-sm filter drop-shadow-sm',
         className
       )}
     >
-      <div className='flex justify-between items-center w-full  px-5 text-sm'>
+      <div className='flex justify-between items-center w-full px-4  md:px-[27px] text-sm'>
         <Logo />
         <ul className='flex space-x-3'>
           {NavLinks.map((link) => (
