dd3dfd7327e59f9cade7075bf206aab5d54a90d3
idil <>
2025-06-29 22:36:58 +0800

    Enhance logo component with hover scale effect

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,9 +7,9 @@ export default function Logo() {
   return (
     <Link
       href={"/"}
-      className="text-zinc-700 text-center hover:text-zinc-600 transition text-lg"
+      className="text-zinc-700 text-center hover:text-zinc-600 transition text-lg hover:scale-105"
     >
-      <div className="bg-zinc-100 w-[100px] h-[50px] flex items-center rounded-sm justify-center hover:shadow-sm transition ">
+      <div className="bg-zinc-100 w-[100px] h-[50px] flex items-center rounded-sm justify-center hover:shadow-sm transition">
         <h1 className={font.className}>Idil Iman</h1>
       </div>
     </Link>
