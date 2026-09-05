7a22acf0414abf803678d3aaa3d748e14a8400dc
idil <idil.iman1999@gmail.com>
2026-09-05 13:55:43 +0800

    test

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    remotePatterns: [
+    remotePatterns:
+    {
+      protocol: "https",
+      hostname: "avatars.githubusercontent.com",
+      pathname: "/u/54613218",
+    },
       {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        pathname: "/u/54613218",
-      },
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-        pathname: "/dms/image/v2/**",
+  protocol: "https",
+    hostname: "media.licdn.com",
+      pathname: "/dms/image/v2/**",
       },
     ],
   }
