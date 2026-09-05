18629c5c397a7bc7bab895a06686e64452ead97a
idil <idil.iman1999@gmail.com>
2026-09-05 13:52:36 +0800

    Enhance personal website layout and content. Added a Hero component for improved presentation, updated metadata with personal details, and configured image optimization for remote sources. Introduced about.json for structured personal information and site.ts for SEO and profile data management.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/u/54613218",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        pathname: "/dms/image/v2/**",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
---
+++ src/components/hero.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function Hero({
+  title,
+  subtitle,
+  description,
+  picture,
+}: {
+  title: string;
+  subtitle: string;
+  description: string;
+  picture: string;
+}) {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-4">
+        <Image
+          src={picture}
+          alt={title}
+          width={56}
+          height={56}
+          className="size-14 shrink-0 rounded-sm object-cover"
+        />
+
+        <div className="flex flex-col gap-0.5">
+          <h1 className="font-mono text-lg">{title}</h1>
+          <h2 className="font-serif text-xl font-medium text-primary">{subtitle}</h2>
+        </div>
+      </div>
+      <p className="max-w-prose text-pretty text-sm/6 text-slate-500">{description}</p>
+    </div>
+  );
+}
