4492dabaaae4b2a4312a2fed6c61227988d9199a
idil <idil.iman1999@gmail.com>
2026-09-05 14:03:28 +0800

    feat: lint staged for ui commit

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,19 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    remotePatterns:
-    {
-      protocol: "https",
-      hostname: "avatars.githubusercontent.com",
-      pathname: "/u/54613218",
-    },
-
-    protocol: "https"
-    hostname: "media.licdn.com",
-    pathname: "/dms/image/v2/**",
-      
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/u/54613218",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        pathname: "/dms/image/v2/**",
+      },
     ],
-}
+  },
 };
 
 export default nextConfig;
